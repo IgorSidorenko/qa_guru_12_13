465c7b3a8f79880d9eda90c78b460e808156198a
igorsid <sidgarri@gmail.com>
2022-07-01 14:04:48 +0800

    fix tests

--- src/test/java/tests/ChangeCityTests.java
+++ src/test/java/tests/ChangeCityTests.java
@@ -4,13 +4,15 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class ChangeCityTests extends TestBase {
 
-    String city = "Иркутск";
+    String city = "Казань";
 
     @Test
     @DisplayName("Изменение города")
@@ -19,7 +21,7 @@ public class ChangeCityTests extends TestBase {
         step("Открыть главную страницу", () -> open("/"));
         step("Открыть форму выбора города", () -> $("[data-selenium=\"cityButton\"]").click());
         step("Ввести название города", () -> $("[data-selenium=\"top-cities-list\"]").$(byText(city)).click());
-        sleep(3000);
+        $("div").shouldBe(Condition.visible, Duration.ofSeconds(10));
         step("Проверить, что город изменился", () -> $("[data-selenium=\"cityButton\"]")
                 .should(Condition.text(city))
                 .should(Condition.visible));
--- src/test/java/tests/OpenArticleTest.java
+++ src/test/java/tests/OpenArticleTest.java
@@ -1,10 +1,9 @@
 package tests;
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class OpenArticleTest extends TestBase {
@@ -12,10 +11,8 @@ public class OpenArticleTest extends TestBase {
     @Test
     @DisplayName("Открытие статьи: Как научиться бегать")
     public void FoundText() {
-        step("Открыть главную страницу", () -> open("/"));
-        step("Нажать на кнопку Читать", () -> $("#span.yk_slot10-block_text-btn").click());
-        step("Проверить, что статья про бег открыта ", () -> $("[data-selenium=\"stk-reset\"]")
-                        .should(Condition.text(" Научиться бегать медленно "))
-                        .should(Condition.visible));
+        step("Открыть статью: Как научиться бегать", () -> open("/media/articles/28972154/"));
+        step("Проверить, что статья про бег открыта ", () -> $("#strong").shouldHave(exactText("Научиться бегать медленно")));
+
     }
 }
