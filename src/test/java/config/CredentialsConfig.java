163491eb3f0c18554a0409a6b53c931b78e6478c
igorsid <sidgarri@gmail.com>
2022-07-26 20:13:34 +0800

    fix tests

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -2,8 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources("classpath:config/credentials.properties")
+@Config.Sources("config/credentials.properties")
 public interface CredentialsConfig extends Config {
     String login();
     String password();
+    String selenoidURL();
 }
